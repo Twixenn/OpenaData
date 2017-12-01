e171e437e072e26c45d99f3423f66bf54e825864
Twixenn <fiagustafsson1998@gmail.com>
2017-12-01 09:16:42 +0100

    Annat test för att kunna köra phantom i webbläsaren, fungerar inte heller

--- js/names.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  //var date = $("input[name='date']").value;
-  //var dates = date.split(" to ");
-  //var from = $("input[name='to']").value;
-  //return [{checkin: dates[0], checkout: dates[1], to: to}];
-  return [{checkin: "2017-11-30", checkout: "2017-12-3", to: "London"}];
-}
