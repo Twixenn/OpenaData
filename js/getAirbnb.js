1f21562c1e690c0e1abcb35cf9391b20492b1aac
Twixenn <fiagustafsson1998@gmail.com>
2017-12-01 09:08:12 +0100

    Sparar informationen från Airbnb i en json-fil

--- js/getAirbnb.js
+++ js/getAirbnb.js
@@ -1,15 +1,20 @@
+var fs = require('fs');
 var webserver = require('webserver');
 var page = require('webpage').create();
 page.settings.userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36";
 //sidans utseende kan ändras om fönstret är för litet (vissa objekt kan vara svårare att få tag på)
 page.viewportSize = {width: 1280, height: 1024};
+//Testar så urlen fungerar om jag sätter in egna värden
+var city = "Berlin";
+var checkin = "2017-11-24";
+var checkout = "2017-11-30";
 
 page.onConsoleMessage = function(msg) {
   console.log(msg);
 }
 
 //öppnar sidan i en "osynlig" webbläsare
-page.open('https://www.airbnb.se/s/Tokyo/homes?refinement_path=%2Fhomes&allow_override%255B%25=&allow_override%5B%5D=&checkin=2017-12-29&checkout=2017-12-30&ne_lat=35.891392701847174&ne_lng=139.9288495501171&sw_lat=35.58850799889009&sw_lng=139.39326605402334&zoom=10&search_by_map=true&s_tag=D931ba6Z', function(status) {
+page.open('https://www.airbnb.se/s/' + city +'/homes?refinement_path=%2Fhomes&allow_override%255B%25=&allow_override%5B%5D=&checkin=' + checkin + '&checkout=' + checkout, function(status) {
   console.log("Status: " + status);
   if(status === "success") {
   } else {
@@ -32,5 +37,8 @@ page.open('https://www.airbnb.se/s/Tokyo/homes?refinement_path=%2Fhomes&allow_ov
     //värderna för de olika boendena är sparade och returneras ihop
     return residents;
   });
+  var path = 'output.json';
+  //sparar informationen i en jsonfil
+  fs.write(path, JSON.stringify(users), 'w');
   phantom.exit();
 });
